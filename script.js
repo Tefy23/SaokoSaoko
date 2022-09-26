30d851cbccaf05b37e6bffb80948f2e5b6a32e76
Estefania Reyes <a301605@uach.mx>
2022-09-26 09:57:29 -0600

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Saoko papi saoko");
+document.getElementById("generation").style.color = "grey";
+function colorTitulo(){
+    console.log("Pulso el botón de Generation");
+    document.getElementById("generation").style.color = "violet";
+}
+
